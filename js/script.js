862d768069fc4258fd756ba1eb354cdfbacdf12e
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-02-28 22:24:46 +0300

    menu to'g'irlandi

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let burger = document.querySelector('.nav__burger');
 let hamburger = document.querySelector('.hamburger');
 let list = document.querySelector('.nav__list');
 let close = document.querySelector('.close');
+let body = document.querySelector('.body');
 
 hamburger.onclick = function(){
     list.style.display = "flex";
