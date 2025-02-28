e80f213e284a0b81b14f4a3f65925e6e2f85cd8f
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-02-28 21:44:37 +0300

    navbarni menusi bulli

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Navbar
+
+let burger = document.querySelector('.nav__burger');
+let hamburger = document.querySelector('.hamburger');
+let list = document.querySelector('.nav__list');
+let close = document.querySelector('.close');
+
+hamburger.onclick = function(){
+    list.style.display = "flex";
+    hamburger.style.display = "none";
+    close.style.display = "block";
+}
+
+close.onclick = function(){
+    list.style.display = "none";
+    hamburger.style.display = "block";
+    close.style.display = "none";
+}
